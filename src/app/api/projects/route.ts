014412e52d71d2fdaa4d45f9e871823d9918e567
Rickyphantom <99458668+Rickyphantom@users.noreply.github.com>
2024-12-19 03:39:05 +0900

    14

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -17,15 +17,15 @@ export async function GET() {
 
 export async function PUT(request: Request) {
   try {
-    const { id, image_url } = await request.json();
+    const { id, name, custom_description, image_url } = await request.json();
     await connectMongoDB();
 
     await Project.findOneAndUpdate({ id }, { image_url }, { upsert: true });
 
-    return NextResponse.json({ message: 'Project image updated' });
+    return NextResponse.json({ message: 'Project updated' });
   } catch {
     return NextResponse.json(
-      { error: 'Failed to update project image' },
+      { error: 'Failed to update project' },
       { status: 500 }
     );
   }
